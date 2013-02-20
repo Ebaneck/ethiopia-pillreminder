cbc637900d5241604178dbdfaa5b90c841c99f5c
Bruce MacLeod <macleod@usm.maine.edu>
2013-02-20 16:40:46 -0500

    Added IVR controller for Adherence callbacks

---
+++ src/main/java/org/motechproject/icappr/content/SoundFiles.java
@@ -0,0 +1,18 @@
+package org.motechproject.icappr.content;
+
+public final class SoundFiles {
+
+    public static final String SOUNDS_DIRECTORY = "/wav/";
+
+    public static final String SOUND_CONTENT_TYPE = "audio/wav";
+
+    public static final String SOUND_EXTENSION = ".wav";
+
+    public static final String PIN_REQUEST = "pinRequest";
+    public static final String INCORRECT_PIN = "incorrectPin";
+    public static final String DOSAGE_ANSWER_YES = "dosageAnswerYes";
+    public static final String DOSAGE_ANSWER_NO = "dosageAnswerNo";
+    public static final String DOSAGE_QUESTION_1 = "dosageQuestion1";
+    public static final String DOSAGE_QUESTION_2 = "dosageQuestion2";
+
+}
