bdbcdc024a7ba404448e888ebc92a953763f10c6
Angela <angela.doxsey@yahoo.com>
2013-03-01 11:46:02 -0500

    Added IVRUIListener, created twiml file to point to IVR UI Decision Tree.

---
+++ src/main/java/org/motechproject/icappr/listener/IVRUIListener.java
@@ -0,0 +1,46 @@
+package org.motechproject.icappr.listener;
+
+import org.motechproject.icappr.events.Events;
+import org.motechproject.icappr.service.CallInitiationService;
+import org.motechproject.icappr.support.DecisionTreeSessionHandler;
+import org.motechproject.event.MotechEvent;
+import org.motechproject.event.listener.annotations.MotechListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IVRUIListener {
+
+	private DecisionTreeSessionHandler decisionTreeSessionHandler;
+	private CallInitiationService callInitiationService;
+
+	@Autowired
+	public IVRUIListener(DecisionTreeSessionHandler decisionTreeSessionHandler,
+			CallInitiationService callInitiationService) {
+		this.decisionTreeSessionHandler = decisionTreeSessionHandler;
+		this.callInitiationService = callInitiationService;
+	}
+
+	@MotechListener(subjects = Events.PATIENT_SELECTED_CONTINUE)
+	public void handleDosageTaken(MotechEvent event) {
+		String sessionId = (event.getParameters().get("flowSessionId")
+				.toString());
+
+		String motechId = decisionTreeSessionHandler
+				.getMotechIdForSessionWithId(sessionId);
+
+		String phoneNum = decisionTreeSessionHandler
+				.getPhoneNumForSessionWithId(event.getParameters()
+						.get("flowSessionId").toString());
+
+		/*We wait one minute before we initiate the next phone call 
+		 */
+		try {
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		callInitiationService.initiateCall(motechId, phoneNum);
+	}
+
+}
--- src/main/java/org/motechproject/icappr/support/IVRUIDecisionTreeBuilder.java
+++ src/main/java/org/motechproject/icappr/support/IVRUIDecisionTreeBuilder.java
@@ -20,93 +20,86 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class IVRUIDecisionTreeBuilder {
-	
-    private final Logger logger = LoggerFactory.getLogger(IVRUIDecisionTreeBuilder.class);
 
-    private final DecisionTreeService decisionTreeService;
-    private final PillReminderSettings settings;
-    private final FlowSessionService flowSessionService;
+	private final Logger logger = LoggerFactory
+			.getLogger(IVRUIDecisionTreeBuilder.class);
 
-    @Autowired
-    public IVRUIDecisionTreeBuilder(DecisionTreeService decisionTreeService, PillReminderSettings settings, FlowSessionService flowSessionService) {
-        this.decisionTreeService = decisionTreeService;
-        this.settings = settings;
-        this.flowSessionService = flowSessionService;
-    }
+	private final DecisionTreeService decisionTreeService;
+	private final PillReminderSettings settings;
+	private final FlowSessionService flowSessionService;
 
-    @PostConstruct
-    public void buildTree() {
-        logger.info("Creating a new ivr-ui-test decision tree");
+	@Autowired
+	public IVRUIDecisionTreeBuilder(DecisionTreeService decisionTreeService,
+			PillReminderSettings settings, FlowSessionService flowSessionService) {
+		this.decisionTreeService = decisionTreeService;
+		this.settings = settings;
+		this.flowSessionService = flowSessionService;
+	}
 
-        // spinning up a new thread so that this will not block the bundle
-        // loading process
-        new Thread(new Runnable() {
+	@PostConstruct
+	public void buildTree() {
+		logger.info("Creating a new ivr-ui-test decision tree");
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				deleteOldTree();
+				createDecisionTree();
+			}
 
-            @Override
-            public void run() {
-                deleteOldTree();
-                createDecisionTree();
-            }
+		}).start();
+	}
 
-        }).start();
-    }
+	private void deleteOldTree() {
+		List<Tree> trees = decisionTreeService.getDecisionTrees();
+		for (Tree tree : trees) {
+			if ("IVRUITestTree".equals(tree.getName())) {
+				decisionTreeService.deleteDecisionTree(tree.getId());
+				break;
+			}
+		}
+	}
 
-    private void deleteOldTree() {
-        List<Tree> trees = decisionTreeService.getDecisionTrees();
-        for (Tree tree : trees) {
-            if ("IVRUITestTree".equals(tree.getName())) {
-                decisionTreeService.deleteDecisionTree(tree.getId());
-                break;
-            }
-        }
-    }
+	private void createDecisionTree() {
+		Tree tree = new Tree();
+		tree.setName("IVRUITestTree");
+		Transition rootTransition = new Transition();
 
-    private void createDecisionTree() {
-        Tree tree = new Tree();
-        tree.setName("IVRUITestTree");
-        Transition rootTransition = new Transition();
+		/*
+		 * The Destination Node is set in such a way that if the user selects
+		 * "1," they will continue to receive calls that prompt them for their
+		 * pin number, re-initiating the whole process.
+		 */
+		rootTransition.setDestinationNode(new Node().setNoticePrompts(
+				new Prompt[] { new AudioPrompt().setAudioFileUrl(settings
+						.getCmsliteUrlFor(SoundFiles.CONTINUE_PROMPTS)) })
+				.setTransitions(
+						new Object[][] { { "1", getContinueTransition() },
+								{ "3", getStopTransition() } }));
+		tree.setRootTransition(rootTransition);
 
-        /*The Destination Node is set in such a way that if the user selects "1," they will 
-         * continue to receive calls that prompt them for their pin number, re-initiating the 
-         * whole process.
-         */
-        rootTransition.setDestinationNode(new Node()
-                .setNoticePrompts(
-                        new Prompt[] { new AudioPrompt().setAudioFileUrl(settings
-                                .getCmsliteUrlFor(SoundFiles.CONTINUE_PROMPTS)) }).setTransitions(
-                        new Object[][] { { "1", getContinueTransition() }, { "3", getStopTransition() } }));
-        tree.setRootTransition(rootTransition);
+		decisionTreeService.saveDecisionTree(tree);
+	}
 
-        decisionTreeService.saveDecisionTree(tree);
-    }
+	private Transition getStopTransition() {
+		EventTransition transition = new EventTransition();
+		transition.setEventSubject(Events.PATIENT_SELECTED_STOP);
+		transition
+				.setDestinationNode(new Node().setPrompts(new AudioPrompt()
+						.setAudioFileUrl(settings
+								.getCmsliteUrlFor(SoundFiles.GOODBYE))));
+		transition.setName("stop");
+		return transition;
+	}
 
-    private Transition getStopTransition() {
-        EventTransition transition = new EventTransition();
-        transition.setEventSubject(Events.PATIENT_SELECTED_STOP);
-        transition.setDestinationNode(new Node().setPrompts(new AudioPrompt().setAudioFileUrl(settings
-                .getCmsliteUrlFor(SoundFiles.GOODBYE))));
-        transition.setName("stop");
-        return transition;
-    }
+	private Transition getContinueTransition() {
+		EventTransition transition = new EventTransition();
+		transition.setEventSubject(Events.PATIENT_SELECTED_CONTINUE);
+		transition
+				.setDestinationNode(new Node().setNoticePrompts(new Prompt[] { new AudioPrompt()
+						.setAudioFileUrl(settings
+								.getCmsliteUrlFor(SoundFiles.CONTINUE_PROMPTS)) }));
+		transition.setName("continue");
+		return transition;
+	}
 
-    private Transition getContinueTransition() {
-        EventTransition transition = new EventTransition();
-        transition.setEventSubject(Events.PATIENT_SELECTED_CONTINUE);
-        transition.setDestinationNode(new Node()
-                .setNoticePrompts(
-                        new Prompt[] { new AudioPrompt().setAudioFileUrl(settings
-                                .getCmsliteUrlFor(SoundFiles.CONTINUE_PROMPTS)) }).setTransitions(
-                        new Object[][] { { "1", getContinueTransition() }, { "3", getStopTransition() } }));
-        transition.setName("continue");
-        reinitiateCalls();
-        return transition;
-    }
-    
-    private void reinitiateCalls(){
-    	/**Somehow we need to get the motech ID and phone number from the call request right here.*/
-    	
-    	//get sessionId somehow
-    	String sessionId = "sessionId";
-    	flowSessionService.getSession(sessionId);
-    }
 }
