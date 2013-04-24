3d7866e7fbc422a3c8c7aa71151430656d5a0765
Russell Gillen <russell.gillenjr@maine.edu>
2013-04-24 13:46:52 -0400

    Added Message handler for RA messages if patient selects 1

---
+++ src/main/java/org/motechproject/icappr/listener/RAMessageHandler.java
@@ -0,0 +1,47 @@
+package org.motechproject.icappr.listener;
+
+import org.motechproject.callflow.domain.FlowSessionRecord;
+import org.motechproject.callflow.service.FlowSessionService;
+import org.motechproject.decisiontree.core.FlowSession;
+import org.motechproject.event.MotechEvent;
+import org.motechproject.event.listener.annotations.MotechListener;
+import org.motechproject.icappr.events.Events;
+import org.motechproject.icappr.mrs.MRSPersonUtil;
+import org.motechproject.icappr.mrs.MrsConstants;
+import org.motechproject.mrs.domain.MRSPatient;
+import org.motechproject.mrs.domain.MRSPerson;
+import org.motechproject.mrs.services.MRSPatientAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RAMessageHandler {
+
+    @Autowired
+    private FlowSessionService flowSessionService;
+    
+    @Autowired
+    private MRSPatientAdapter patientAdapter;
+    
+    @MotechListener( subjects = {Events.PATIENT_WANTS_CLINIC_CALL } )
+    public void sendRAMessage(MotechEvent event) {
+        String flowSessionId = (String) event.getParameters().get("flowSessionId");
+        FlowSessionRecord flowSession = (FlowSessionRecord) flowSessionService.getSession(flowSessionId);
+        if (flowSession != null) {
+            String motechId = flowSession.get("motechId");
+            MRSPatient patient = patientAdapter.getPatientByMotechId(motechId);
+            if (patient != null) {
+                MRSPerson person = patient.getPerson();
+                String phoneNumber = MRSPersonUtil.getAttrValue(MrsConstants.PERSON_PHONE_NUMBER_ATTR, patient.getPerson().getAttributes());
+                String firstName = person.getFirstName();
+                String lastName = person.getLastName();
+                buildThenSendMessage(phoneNumber, firstName, lastName);
+            }
+        }
+    }
+
+    private void buildThenSendMessage(String phoneNumber, String firstName, String lastName) {
+        // TODO Auto-generated method stub
+        
+    }
+}
