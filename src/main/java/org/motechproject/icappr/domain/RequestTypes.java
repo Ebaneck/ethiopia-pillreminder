5fc7bf30c36df6fd5faca5f6d210a8a160f44165
Russell Gillen <russell.gillenjr@maine.edu>
2013-04-29 17:53:40 -0400

    New type of request

--- src/main/java/org/motechproject/icappr/domain/RequestTypes.java
+++ src/main/java/org/motechproject/icappr/domain/RequestTypes.java
@@ -1,9 +1,10 @@
 package org.motechproject.icappr.domain;
 
 public interface RequestTypes {
-	
-	public static final String IVR_UI = "ivrUIType";
-	public static final String ADHERENCE_CALL = "adherenceCall";
+
+    public static final String IVR_UI = "ivrUIType";
+    public static final String ADHERENCE_CALL = "adherenceCall";
     public static final String PILL_REMINDER_CALL = "pillReminderCall";
-	
+    public static final String SIDE_EFFECT_CALL = "sideEffectCall";
+
 }
