b59fbc7cdc5c02d86b764475433ee5eddc77e7bb
Russell Gillen <russell.gillenjr@maine.edu>
2013-04-29 17:54:53 -0400

    Updating call tester for side effects

--- src/main/java/org/motechproject/icappr/web/CallTester.java
+++ src/main/java/org/motechproject/icappr/web/CallTester.java
@@ -10,8 +10,11 @@ import org.joda.time.DateTime;
 import org.motechproject.commons.date.util.DateUtil;
 import org.motechproject.icappr.PillReminderSettings;
 import org.motechproject.icappr.domain.IVREnrollmentRequest;
+import org.motechproject.icappr.domain.Request;
+import org.motechproject.icappr.domain.SideEffectCallEnrollmentRequest;
 import org.motechproject.icappr.mrs.MRSPersonUtil;
 import org.motechproject.icappr.mrs.MrsConstants;
+import org.motechproject.icappr.service.CallInitiationService;
 import org.motechproject.icappr.service.IVRUIEnroller;
 import org.motechproject.icappr.support.CallRequestDataKeys;
 import org.motechproject.ivr.service.CallRequest;
@@ -33,7 +36,24 @@ public class CallTester {
     @Autowired
     private IVRUIEnroller enroller;
 
+    @Autowired
+    private CallInitiationService callService;
 
+    @RequestMapping("/sideeffects")
+    @ResponseBody
+    public String testSideEffects(HttpServletRequest request) {
+        String phoneNumber = request.getParameter("phone");
+
+        Request pinRequest = new SideEffectCallEnrollmentRequest();
+        pinRequest.setLanguage("english");
+        pinRequest.setMotechID("1984");
+        pinRequest.setPhoneNumber(phoneNumber);
+        pinRequest.setPin("1234");
+
+        callService.initiateCall(pinRequest);
+
+        return "success";
+    }
 
     @RequestMapping("/testcall")
     @ResponseBody
