ab84cafe5c169e791fa560bc41a0449ee6805269
Russell <russell.gillenjr@maine.edu>
2013-05-02 16:38:57 -0400

    Renamed controller, started work on interaction with other calls

--- src/main/java/org/motechproject/icappr/web/VerboiceInteractionController.java
+++ src/main/java/org/motechproject/icappr/web/VerboiceInteractionController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.EventRelay;
+import org.motechproject.icappr.PillReminderSettings;
 import org.motechproject.icappr.events.Events;
 import org.motechproject.server.config.SettingsFacade;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class ManifestController {
+public class VerboiceInteractionController {
 
     @Autowired
     private SettingsFacade facade;
+
+    @Autowired
+    private PillReminderSettings settings;
 
     @Autowired
     private EventRelay eventRelay;
@@ -35,7 +39,9 @@ public class ManifestController {
     @ResponseBody
     public String manifest(HttpServletRequest request) throws IOException {
         InputStream manifest = facade.getRawConfig("icapprManifest.xml");
-        return IOUtils.toString(manifest);
+        String manifestBody = IOUtils.toString(manifest);
+        String host = settings.getMotechUrl() + "/module/icappr";
+        return manifestBody.replace("HOST", host);
     }
 
     @RequestMapping("/answer")
@@ -48,12 +54,34 @@ public class ManifestController {
             //raise event here
             String eventToRaise = null;
             switch (question) {
-            case "question1" : eventToRaise = Events.YES_YELLOW_SKIN_OR_EYES; break;
-            case "question2" : eventToRaise = Events.YES_ABDOMINAL_PAIN_OR_VOMITING; break;
-            case"question3" : eventToRaise = Events.YES_SKIN_RASH_OR_ITCHY_SKIN; break;
-            case "question4" : eventToRaise = Events.TINGLING_OR_NUMBNESS_OF_HANDS_OR_FEET; break;
+                case "question1" : eventToRaise = Events.YES_YELLOW_SKIN_OR_EYES; break;
+                case "question2" : eventToRaise = Events.YES_ABDOMINAL_PAIN_OR_VOMITING; break;
+                case"question3" : eventToRaise = Events.YES_SKIN_RASH_OR_ITCHY_SKIN; break;
+                case "question4" : eventToRaise = Events.TINGLING_OR_NUMBNESS_OF_HANDS_OR_FEET; break;
             }
 
+            MotechEvent event = new MotechEvent(eventToRaise);
+            event.getParameters().put("flowSessionId", callSid);
+
+            eventRelay.sendEventMessage(event);
+        }
+
+        return "success";
+    }
+
+    @RequestMapping("patientConcern")
+    @ResponseBody
+    public String adherenceAnswer(HttpServletRequest request) {
+        String choice = request.getParameter("answer");
+        String callSid = request.getParameter("CallSid");
+        String concern = request.getParameter("concernType");
+        if ("1".equals(choice)) {
+            String eventToRaise = null;
+            switch (concern) {
+                case "adherenceConcern" : eventToRaise = Events.SEND_RA_MSSAGE_ADHERENCE_CONCERNS; break;
+                case "appointmentConcern" : eventToRaise = Events.SEND_RA_MESSAGE_APPOINTMENT_CONCERNS; break;
+
+            }
             MotechEvent event = new MotechEvent(eventToRaise);
             event.getParameters().put("flowSessionId", callSid);
 
