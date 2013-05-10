2615ce14cd54bae12d0cd52ee17d90acca5af546
Russell <russell.gillenjr@maine.edu>
2013-05-10 15:12:48 -0400

    Time testing controller enhancements

--- src/main/java/org/motechproject/icappr/web/TimeTesterController.java
+++ src/main/java/org/motechproject/icappr/web/TimeTesterController.java
@@ -2,6 +2,10 @@ package org.motechproject.icappr.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.joda.time.DateTime;
+import org.joda.time.chrono.EthiopicChronology;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +13,53 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TimeTesterController {
 
+    private Logger logger = LoggerFactory.getLogger("motech-icappr");
+
     @RequestMapping("/timetest")
     @ResponseBody
-    public void testTime(HttpServletRequest httpServletRequest) {
+    public String testTime(HttpServletRequest request) {
 
+        String monthsToAdd = request.getParameter("months");
+        String daysToAdd = request.getParameter("days");
+        String hoursToAdd = request.getParameter("hours");
+        String minutesToAdd = request.getParameter("minutes");
+
+
+        StringBuilder returnString = new StringBuilder("Gregorian vs Ethiopian dates");
+
+        EthiopicChronology ethiopiaChronology = EthiopicChronology.getInstance();
+
+        DateTime gregorian = DateTime.now();
+        DateTime ethiopian = DateTime.now(ethiopiaChronology);
+
+        returnString.append("Time in Gregorian: " + gregorian.toString() + " day of week: " + gregorian.getDayOfWeek() + " month of year" + gregorian.getMonthOfYear() + " and year: " + gregorian.getYear() + "\r\n");
+        returnString.append("Time in Ethiopia: " + ethiopian.toString() +  " day of week: " + ethiopian.getDayOfWeek() + " month of year" + ethiopian.getMonthOfYear() + " and year: " + ethiopian.getYear() + "\r\n");
+
+        gregorian = transformTime(gregorian, monthsToAdd, daysToAdd, hoursToAdd, minutesToAdd);
+        ethiopian = transformTime(ethiopian, monthsToAdd, daysToAdd, hoursToAdd, minutesToAdd);
+
+        returnString.append("Time in Gregorian: " + gregorian.toString() + " day of week: " + gregorian.getDayOfWeek() + " month of year" + gregorian.getMonthOfYear() + " and year: " + gregorian.getYear() + "\r\n");
+        returnString.append("Time in Ethiopia: " + ethiopian.toString() +  " day of week: " + ethiopian.getDayOfWeek() + " month of year" + ethiopian.getMonthOfYear() + " and year: " + ethiopian.getYear() + "\r\n");
+
+        return returnString.toString();
+
+    }
+
+    private DateTime transformTime(DateTime time, String monthsToAdd, String daysToAdd, String hoursToAdd, String minutesToAdd) {
+
+        if (monthsToAdd != null) {
+            time = time.plusMonths(Integer.parseInt(monthsToAdd));
+        }
+        if (daysToAdd != null) {
+            time = time.plusDays(Integer.parseInt(daysToAdd));
+        }
+        if (hoursToAdd != null) {
+            time = time.plusHours(Integer.parseInt(hoursToAdd));
+        }
+        if (minutesToAdd != null) {
+            time = time.plusMinutes(Integer.parseInt(minutesToAdd));
+        }
+
+        return time;
     }
 }
